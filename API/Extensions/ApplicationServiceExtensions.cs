d4868459591493dda682767759039075e2cdb69f
Dustin Vu <dqvu0320@gmail.com>
2023-04-10 13:47:38 -0500

    End of section 19

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -18,17 +18,20 @@ namespace API.Extensions
       // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore swashbuckle
       services.AddEndpointsApiExplorer();
       services.AddSwaggerGen();
+      
       services.AddDbContext<DataContext>(opt =>
       {
         opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
       });
+
       services.AddCors(opt =>
       {
         opt.AddPolicy("CorsPolicy", policy =>
         {
-          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+          policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:3000");
         });
       });
+
       services.AddMediatR(typeof(List.Handler));
       services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
@@ -40,6 +43,8 @@ namespace API.Extensions
 
       services.AddScoped<IPhotoAccessor, PhotoAccessor>();
       services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+
+      services.AddSignalR();
 
       return services;
     }
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
     public DbSet<Activity> Activities { get; set; }
     public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
     public DbSet<Photo> Photos { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 
     // Manually create join table
     protected override void OnModelCreating(ModelBuilder builder)
@@ -20,18 +21,24 @@ namespace Persistence
       base.OnModelCreating(builder);
 
       // Combine two keys together to form primary key for table
-      builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new {aa.AppUserId, aa.ActivityId}));
+      builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
 
       // Create many - many relationship
       builder.Entity<ActivityAttendee>()
         .HasOne(u => u.AppUser)
         .WithMany(a => a.Activities)
         .HasForeignKey(aa => aa.AppUserId);
-      
+
       builder.Entity<ActivityAttendee>()
         .HasOne(u => u.Activity)
         .WithMany(a => a.Attendees)
         .HasForeignKey(aa => aa.ActivityId);
+
+      // When we delete activity, associated comments with activity will be cascaded
+      builder.Entity<Comment>()
+        .HasOne(a => a.Activity)
+        .WithMany(c => c.Comments)
+        .OnDelete(DeleteBehavior.Cascade);
     }
   }
 }
