732d9f9e8b890c2522274ecee9468b6831d02ca8
Dustin Vu <dqvu0320@gmail.com>
2023-04-14 14:31:23 -0500

    Pre-deploy

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -5,27 +5,31 @@ import ActivityForm from "../../features/activities/form/ActivityForm";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import TestError from "../../features/errors/TestError";
-import LoginForm from "../../features/users/LoginForm";
 import App from "../layout/App";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
 export const routes: RouteObject[] = [
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
-      {path: 'activities', element: <ActivityDashboard/>},
-      {path: 'activities/:id', element: <ActivityDetails/>},
-      {path: 'createActivity', element: <ActivityForm key='create' />},
-      {path: 'manage/:id', element: <ActivityForm key='manage' />},
-      {path: 'profiles/:username', element: <ProfilePage />},
-      {path: 'login', element: <LoginForm />},
-      {path: 'errors', element: <TestError />},
-      {path: 'not-found', element: <NotFound />},
-      {path: 'server-error', element: <ServerError />},
-      {path: '*', element: <Navigate replace to='/not-found' />},
-    ]
-  }
-]
+      {
+        element: <RequireAuth />,
+        children: [
+          { path: "activities", element: <ActivityDashboard /> },
+          { path: "activities/:id", element: <ActivityDetails /> },
+          { path: "createActivity", element: <ActivityForm key="create" /> },
+          { path: "manage/:id", element: <ActivityForm key="manage" /> },
+          { path: "profiles/:username", element: <ProfilePage /> },
+          { path: "errors", element: <TestError /> },
+        ],
+      },
+      { path: "not-found", element: <NotFound /> },
+      { path: "server-error", element: <ServerError /> },
+      { path: "*", element: <Navigate replace to="/not-found" /> },
+    ],
+  },
+];
 
 export const router = createBrowserRouter(routes);
--- client-app/src/app/stores/commentStore.ts
+++ client-app/src/app/stores/commentStore.ts
@@ -15,7 +15,7 @@ export default class CommentStore {
     if (store.activityStore.selectedActivity) {
       // Pass in a string "activityId" in http 
       this.hubConnection = new HubConnectionBuilder()
-        .withUrl('http://localhost:5000/chat?activityId=' + activityId, {
+        .withUrl(process.env.REACT_APP_CHAT_URL + '?activityId=' + activityId, {
           accessTokenFactory: () => store.userStore.user?.token!
         })
         .withAutomaticReconnect()
@@ -29,7 +29,7 @@ export default class CommentStore {
       this.hubConnection.on('LoadComments', (comments: ChatComment[]) => {
         runInAction(() => {
           comments.forEach(comment => {
-            comment.createdAt = new Date(comment.createdAt + 'Z');
+            comment.createdAt = new Date(comment.createdAt);
           })
           this.comments = comments;
         }); 
